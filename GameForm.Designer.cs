dea14902e307bfacb33311d569c8dacda7cd85b4
OstapRedneb <rodionzabirov888@gmail.com>
2025-08-19 14:16:32 +0500

    Добавьте файлы проекта.

---
+++ GameForm.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace AngryBirdsWinFormsApp
+{
+    partial class GameForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groundLabel = new Label();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            scoreLabel = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            label14 = new Label();
+            label15 = new Label();
+            label16 = new Label();
+            label17 = new Label();
+            label18 = new Label();
+            label19 = new Label();
+            label20 = new Label();
+            SuspendLayout();
+            // 
+            // groundLabel
+            // 
+            groundLabel.BackColor = Color.FromArgb(128, 64, 0);
+            groundLabel.Location = new Point(-1, 389);
+            groundLabel.Name = "groundLabel";
+            groundLabel.Size = new Size(803, 61);
+            groundLabel.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.BackColor = Color.FromArgb(0, 64, 0);
+            label1.Location = new Point(-1, 374);
+            label1.Name = "label1";
+            label1.Size = new Size(803, 15);
+            label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.BackColor = Color.FromArgb(64, 32, 0);
+            label2.Location = new Point(77, 317);
+            label2.Name = "label2";
+            label2.Size = new Size(14, 72);
+            label2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.BackColor = Color.FromArgb(64, 32, 0);
+            label3.Location = new Point(77, 316);
+            label3.Name = "label3";
+            label3.Size = new Size(28, 10);
+            label3.TabIndex = 4;
+            // 
+            // label4
+            // 
+            label4.BackColor = Color.FromArgb(64, 32, 0);
+            label4.Location = new Point(97, 295);
+            label4.Name = "label4";
+            label4.Size = new Size(14, 31);
+            label4.TabIndex = 5;
+            // 
+            // label5
+            // 
+            label5.BackColor = Color.FromArgb(64, 32, 0);
+            label5.Location = new Point(63, 317);
+            label5.Name = "label5";
+            label5.Size = new Size(28, 10);
+            label5.TabIndex = 6;
+            // 
+            // label6
+            // 
+            label6.BackColor = Color.FromArgb(64, 32, 0);
+            label6.Location = new Point(63, 295);
+            label6.Name = "label6";
+            label6.Size = new Size(14, 31);
+            label6.TabIndex = 7;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Comic Sans MS", 18F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label7.ForeColor = Color.White;
+            label7.Location = new Point(12, 9);
+            label7.Name = "label7";
+            label7.Size = new Size(89, 35);
+            label7.TabIndex = 8;
+            label7.Text = "очки: ";
+            // 
+            // scoreLabel
+            // 
+            scoreLabel.AutoSize = true;
+            scoreLabel.Font = new Font("Comic Sans MS", 18F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            scoreLabel.ForeColor = Color.White;
+            scoreLabel.Location = new Point(97, 9);
+            scoreLabel.Name = "scoreLabel";
+            scoreLabel.Size = new Size(30, 35);
+            scoreLabel.TabIndex = 9;
+            scoreLabel.Text = "0";
+            // 
+            // label8
+            // 
+            label8.BackColor = Color.FromArgb(64, 64, 64);
+            label8.Location = new Point(23, 427);
+            label8.Name = "label8";
+            label8.Size = new Size(25, 14);
+            label8.TabIndex = 10;
+            // 
+            // label9
+            // 
+            label9.BackColor = Color.FromArgb(64, 64, 64);
+            label9.Location = new Point(80, 398);
+            label9.Name = "label9";
+            label9.Size = new Size(25, 14);
+            label9.TabIndex = 11;
+            // 
+            // label10
+            // 
+            label10.BackColor = Color.FromArgb(64, 64, 64);
+            label10.Location = new Point(131, 427);
+            label10.Name = "label10";
+            label10.Size = new Size(25, 14);
+            label10.TabIndex = 12;
+            // 
+            // label11
+            // 
+            label11.BackColor = Color.FromArgb(64, 64, 64);
+            label11.Location = new Point(218, 398);
+            label11.Name = "label11";
+            label11.Size = new Size(25, 14);
+            label11.TabIndex = 13;
+            // 
+            // label12
+            // 
+            label12.BackColor = Color.FromArgb(64, 64, 64);
+            label12.Location = new Point(255, 427);
+            label12.Name = "label12";
+            label12.Size = new Size(25, 14);
+            label12.TabIndex = 14;
+            // 
+            // label13
+            // 
+            label13.BackColor = Color.FromArgb(64, 64, 64);
+            label13.Location = new Point(348, 398);
+            label13.Name = "label13";
+            label13.Size = new Size(25, 14);
+            label13.TabIndex = 15;
+            // 
+            // label14
+            // 
+            label14.BackColor = Color.FromArgb(64, 64, 64);
+            label14.Location = new Point(375, 427);
+            label14.Name = "label14";
+            label14.Size = new Size(25, 14);
+            label14.TabIndex = 16;
+            // 
+            // label15
+            // 
+            label15.BackColor = Color.FromArgb(64, 64, 64);
+            label15.Location = new Point(528, 398);
+            label15.Name = "label15";
+            label15.Size = new Size(25, 14);
+            label15.TabIndex = 17;
+            // 
+            // label16
+            // 
+            label16.BackColor = Color.FromArgb(64, 64, 64);
+            label16.Location = new Point(487, 427);
+            label16.Name = "label16";
+            label16.Size = new Size(25, 14);
+            label16.TabIndex = 18;
+            // 
+            // label17
+            // 
+            label17.BackColor = Color.FromArgb(64, 64, 64);
+            label17.Location = new Point(641, 389);
+            label17.Name = "label17";
+            label17.Size = new Size(25, 14);
+            label17.TabIndex = 19;
+            // 
+            // label18
+            // 
+            label18.BackColor = Color.FromArgb(64, 64, 64);
+            label18.Location = new Point(676, 427);
+            label18.Name = "label18";
+            label18.Size = new Size(25, 14);
+            label18.TabIndex = 20;
+            // 
+            // label19
+            // 
+            label19.BackColor = Color.FromArgb(64, 64, 64);
+            label19.Location = new Point(586, 427);
+            label19.Name = "label19";
+            label19.Size = new Size(25, 14);
+            label19.TabIndex = 21;
+            // 
+            // label20
+            // 
+            label20.BackColor = Color.FromArgb(64, 64, 64);
+            label20.Location = new Point(747, 398);
+            label20.Name = "label20";
+            label20.Size = new Size(25, 14);
+            label20.TabIndex = 22;
+            // 
+            // GameForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Teal;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label20);
+            Controls.Add(label19);
+            Controls.Add(label18);
+            Controls.Add(label17);
+            Controls.Add(label16);
+            Controls.Add(label15);
+            Controls.Add(label14);
+            Controls.Add(label13);
+            Controls.Add(label12);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(scoreLabel);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(groundLabel);
+            Name = "GameForm";
+            Text = "AngryBirds";
+            Load += GameForm_Load;
+            MouseDown += GameForm_MouseDown;
+            MouseMove += GameForm_MouseMove;
+            MouseUp += GameForm_MouseUp;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Label groundLabel;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label scoreLabel;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private Label label13;
+        private Label label14;
+        private Label label15;
+        private Label label16;
+        private Label label17;
+        private Label label18;
+        private Label label19;
+        private Label label20;
+    }
+}
