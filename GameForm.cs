f8cc4b832f7942270c60bcffffd0cbde86f7edca
OstapRedneb <rodionzabirov888@gmail.com>
2025-08-19 18:37:49 +0500

    Фикс натягивания мшкой и отображения

--- GameForm.cs
+++ GameForm.cs
@@ -8,14 +8,16 @@ namespace AngryBirdsWinFormsApp
         static Graphics graphics;
         static Random random = new Random();
 
-        static Bird bird = new Bird(63, 385, 30, 0, 0, 0.2f, Color.Red);
-        static Pig pig = new Pig(random.Next(280, 280 + 454), random.Next(49, 49 + 261), 30, 0, 0, 0, Color.Green);
+        static Bird bird;
+        static Pig pig;
 
         static bool isStrech = false;
         static bool isUsing = false;
 
         public GameForm()
         {
+            InitializeComponent();
+
             graphics = CreateGraphics();
 
             Ball.graphics = graphics;
@@ -23,21 +25,12 @@ namespace AngryBirdsWinFormsApp
             Ball.formPen = new Pen(BackColor);
             Ball.formBrush = new SolidBrush(BackColor);
             Ball.form = this;
-
-
-            bird.Show();
-            pig.Show();
-
-            bird.timer.Start();
-            pig.timer.Start();
-
-            InitializeComponent();
         }
 
         private void GameForm_Load(object sender, EventArgs e)
         {
-            Ball ball = new Ball(10, 10, 100, 0, 0);
-            ball.Show();
+            bird = new Bird(63, 285, 30, 0, 0, 0, Color.Red);
+            pig = new Pig(random.Next(280, 280 + 454), random.Next(49, 49 + 261), 30, 0, 0, 0, Color.Green);
 
             bird.Show();
             pig.Show();
@@ -48,15 +41,41 @@ namespace AngryBirdsWinFormsApp
             if (e.Button == MouseButtons.Left && Math.Pow(e.X - bird.centerX, 2) + Math.Pow(e.Y - bird.centerY, 2) <= MathF.Pow(bird.radius, 2) && !isUsing)
             {
                 isStrech = true;
-                bird.x = e.Location.X - bird.radius;
-                bird.y = e.Location.Y - bird.radius;
-
             }
         }
         public void GameForm_MouseMove(object sender, MouseEventArgs e)
         {
             if (isStrech)
             {
+                bird.Clear();
+
+                bird.x = e.Location.X - bird.radius;
+                bird.y = e.Location.Y - bird.radius;
+
+                if (bird.x < 23)
+                {
+                    bird.x = 23;
+                    //bird.y = e.Location.Y - bird.radius;
+                }
+
+                if (bird.x > 103)
+                {
+                    bird.x = 103;
+                    //bird.y = e.Location.Y - bird.radius;
+                }
+
+
+                if (bird.y > 285 + 40)
+                {
+                    bird.y = 285 + 40;
+                    //bird.x = e.Location.X - bird.radius;
+                }
+
+                if (bird.y < 285 - 40)
+                {
+                    bird.y = 285 - 40;
+                    //bird.x = e.Location.X - bird.radius;
+                }
 
             }
         }
@@ -64,8 +83,9 @@ namespace AngryBirdsWinFormsApp
         {
             isStrech = false;
             isUsing = true;
+
         }
 
-        
+
     }
 }
